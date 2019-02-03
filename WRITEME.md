eaf5b16b717e79a8a4d7b7f5a418c1731efca062
Dipak Yadav <dyadav@ashoka.org>
2019-02-03 15:41:25 +0530

    Read & Write from filesystem.

---
+++ WRITEME.md
@@ -0,0 +1,2 @@
+# node-js-activities
+Each activity is in a commit, so don't bother looking for all activities at top of master.
--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-var events = require('events');
-var util = require('util');
+var fs = require('fs');
 
-var Person = function (name) {
-  this.name = name
-};
-
-util.inherits(Person, events.EventEmitter);
-
-var James = new Person('James');
-var Jill = new Person('Jill');
-var Johan = new Person('Johan');
-
-var people = [James, Jill, Johan];
-
-people.forEach((person) => person.on('speak', (message) => console.log(`${person.name} said: ${message}`)));
-
-James.emit('speak', 'Hey Jill, Hey Johan!!')
+fs.readFile(__dirname + '/README.md', 'utf-8', (error, data) => fs.writeFileSync(__dirname + '/WRITEME.md', data));
