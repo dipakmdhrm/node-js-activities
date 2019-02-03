ff246ef1f3e599d3643f87d9800e57db8d5ebb4b
Dipak Yadav <dyadav@ashoka.org>
2019-02-03 13:39:29 +0530

    Used function expressions.

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-// Say hello.
-console.log('Hello world!');
+// Normal functions statements.
+function sayHello() {
+  console.log('Hello world!');
+}
 
-// Say hello again after 1 second.
-setTimeout(function() {
-  console.log('Hello again! It\'s been so long! Like 1 second long.')
-}, 1000)
+sayHello();
 
-// Say hello repeatedly after every second for next 5 seconds.
-var time = 0;
-var timer = setInterval(function(){
-  time += 1;
-  console.log('Hello again after ' + time + ' seconds!')
-  if (time >= 5) {
-    clearInterval(timer);
-  }
-}, 1000);
+// Function expressions.
+var sayBye = function() {
+  console.log('Bye world!');
+}
+
+sayBye();
+
+// Function inside function.
+function callFun(fun) {
+  fun();
+}
+
+callFun(sayBye);
