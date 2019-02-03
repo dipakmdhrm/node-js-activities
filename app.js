8a9e68f6730ef424c87c70473fe141c2aecdf2db
Dipak Yadav <dyadav@ashoka.org>
2019-02-03 13:25:55 +0530

    Used time function available on global object.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+// Say hello.
 console.log('Hello world!');
+
+// Say hello again after 1 second.
+setTimeout(function() {
+  console.log('Hello again! It\'s been so long! Like 1 second long.')
+}, 1000)
+
+// Say hello repeatedly after every second for next 5 seconds.
+var time = 0;
+var timer = setInterval(function(){
+  time += 1;
+  console.log('Hello again after ' + time + ' seconds!')
+  if (time >= 5) {
+    clearInterval(timer);
+  }
+}, 1000);
