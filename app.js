53a8af1b70babdafc3fa5b5af6d27e93477f61a2
Dipak Yadav <dyadav@ashoka.org>
2019-02-03 14:26:51 +0530

    Used module.export & require with arrow functions.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-var sum = require (__dirname + '/sum');
+var basicMaths = require (__dirname + '/basicMaths');
 
-console.log(sum([1, 2, 3, 4, 5]));
+console.log(basicMaths.adder([1, 2, 3, 4, 5]));
+console.log(basicMaths.subtractor(5, 1));
+console.log(basicMaths.multiplier([1, 2, 3, 4, 5]));
+console.log(basicMaths.divider(6, 2));
--- sum.js
+++
@@ -1,9 +0,0 @@
-function sum(arr) {
-  return arr.reduce(reducer);
-}
-
-function reducer(accumulator, currentValue) {
-  return accumulator + currentValue;
-}
-
-module.exports = sum;
