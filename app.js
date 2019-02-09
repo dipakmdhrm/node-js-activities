fffd57ab17a08a6ca4aca4ecabe22de2ed6dd020
Dipak Yadav <dyadav@ashoka.org>
2019-02-09 11:21:08 +0530

    Used express middleware to serve static files.

--- app.js
+++ app.js
@@ -8,14 +8,20 @@ app.set('view engine', 'ejs');
 // Start the server.
 app.listen(3000);
 
+// Static files.
+app.use('/assets', express.static('assets'));
+
+// Home page.
 app.get('/', function(request, response){
   response.render('index');
 });
 
+// Contact page.
 app.get('/contact', function (request, response) {
   response.render('contact');
 });
 
+// Profile page.
 app.get('/profile/:name', function (request, response) {
   var data = {
     age: 28,
